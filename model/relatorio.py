61e1f1235eaba9ebe0e5b2755aece2cf5303f8be
Jeanzhn <vieiragustavo014@gmail.com>
2025-04-04 15:24:17 -0300

    versao 0.0.1

--- model/relatorio.py
+++ model/relatorio.py
@@ -1,3 +1,5 @@
+from model import *
+
 def relatorio():
     def relatorio_func():
-        
+    
---
+++ model/to_json.py
@@ -0,0 +1,9 @@
+def To_json():
+    def to_json(self, saida):
+        try:
+            json.dump(self.__dict__)
+            with open(saida, 'w') as file:
+                json.dump(self.__dict__, file, indent=4, ensure_ascii=False) #converte o dicionário em JSON e salva no arquivo
+        except TypeError as e:
+            raise TypeError(f"Erro ao converter para JSON: {e}")
+        
